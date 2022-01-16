4bb33778e8a69a3aeb6f66b2a58b5e2efe4972db
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-01-16 18:35:03 -0400

    Complete TwoSum in csharp

---
+++ Leetcode/TwoSum.cs
@@ -0,0 +1,14 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.Length; i++)
+            for (int j = 0; j < nums.Length; j++)
+                if (i != j && nums[i] + nums[j] == target)
+                    return new int[] {i, j};
+
+        return new int[] {0, 1};
+    }
+}
