613b5cc97cce446d79434111e388827edde8f5fa
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-01-19 07:40:55 -0400

    Complete RomanToInteger in python

--- Leetcode/RomanToInteger.py
+++ Leetcode/RomanToInteger.py
@@ -1,8 +1,10 @@
+# Given a roman numeral, convert it to an integer.
+
 def romanToInt(s):
     roman_map = {"I": 1, "V": 5, "X": 10,
                  "L": 50, "C": 100, "D": 500, "M": 1000}
     roman_int = 0
-    
+
     for i in range(len(s)):
         if i + 1 < len(s) and roman_map[s[i]] < roman_map[s[i + 1]]:
             roman_int -= roman_map[s[i]]
