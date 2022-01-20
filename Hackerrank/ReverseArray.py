daba7d5cc2004f81a152f37364679f4a0e3007c5
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-01-20 10:07:54 -0400

    Refactor ReverseArray in python

--- Hackerrank/ReverseArray.py
+++ Hackerrank/ReverseArray.py
@@ -1,8 +1,4 @@
 # Reverse an array of integers
 
 def reverseArray(array):
-    rev_array = []
-    for i in range(len(array) - 1, -1, -1):
-        rev_array.append(array[i])
-    
-    return rev_array
+    return array[::-1]
