44bf8ae31cf2dca0da69c13f89b70c95500f730e
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-01-23 19:38:29 -0400

    Refactor RemoveElement in csharp

--- Leetcode/RemoveElement.cs
+++ Leetcode/RemoveElement.cs
@@ -11,11 +11,13 @@ public class Solution
         int k = 0;
         
         for (int i = 0; i < nums.Length; i++)
+        {
             if (nums[i] != val)
             {
                 nums[k] = nums[i];
                 k++;
             }
+        }
         
         return k;
     }
