f2df25451c027b30589773661e7657b9eadcc581
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-01-24 18:57:38 -0400

    Complete ImplementStr in python

---
+++ Leetcode/ImplementStr.py
@@ -0,0 +1,30 @@
+# Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+def strStr(self, haystack, needle):
+    if len(needle) == 0:
+        return 0
+        
+    for i in range(len(haystack) - len(needle) + 1):
+        if haystack[i:i + len(needle)] == needle:
+            return i
+            
+    return -1
+
+# The following solution works but won't be accepted by Leetcode due to a time limit
+
+def strStr(self, haystack, needle):
+    if len(needle) == 0:
+        return 0
+    elif len(haystack) < len(needle):
+        return -1
+        
+    for i in range(len(haystack) - len(needle) + 1):
+        if haystack[i] == needle[0]:
+            for j in range(len(needle)):
+                if haystack[j + i] != needle[j]:
+                    break
+                if j == len(needle) - 1:
+                    return i
+                j += 1
+                
+    return -1
