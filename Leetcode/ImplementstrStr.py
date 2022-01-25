5a20bda80f4577ffc846b800c4cd5c04e7b58912
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-01-25 15:53:04 -0400

    Refactor ImplementstrStr in python

--- Leetcode/ImplementstrStr.py
+++ Leetcode/ImplementstrStr.py
@@ -9,20 +9,3 @@ def strStr(self, haystack, needle):
             return i
             
     return -1
-
-# The following solution is valid but won't be accepted by Leetcode
-
-def strStr(self, haystack, needle):
-    if len(needle) == 0:
-        return 0
-    elif len(haystack) < len(needle):
-        return -1
-        
-    for i in range(len(haystack) - len(needle) + 1):
-        if haystack[i] == needle[0]:
-            for j in range(len(needle)):
-                if haystack[j + i] != needle[j]:
-                    break
-                if j == len(needle) - 1:
-                    return i
-    return -1
