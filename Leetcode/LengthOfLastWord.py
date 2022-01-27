56c1c576d98250c66cb5c792acdd5b3272b0df72
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-01-27 12:08:07 -0400

    Refactor LengthOfLastWord in python

--- Leetcode/LengthOfLastWord.py
+++ Leetcode/LengthOfLastWord.py
@@ -5,7 +5,7 @@ def lengthOfLastWord(self, s):
     
     while s[i] == ' ':
         i -= 1
-    while s[i] != ' ' and i >= 0:
+    while i >= 0 and s[i] != ' ':
         length += 1
         i -= 1
     
