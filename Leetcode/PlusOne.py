63792e0f7d4cf64c105cce4cf034a52adc9cef62
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-01-28 12:40:17 -0400

    Complete PlusOne in python

---
+++ Leetcode/PlusOne.py
@@ -0,0 +1,15 @@
+# You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
+# Increment the large integer by one and return the resulting array of digits.
+
+def plusOne(self, digits):
+    i = len(digits) - 1
+    
+    while i >= 0:
+        if digits[i] == 9:
+            digits[i] = 0
+        else:
+            digits[i] += 1
+            return digits
+        i -= 1
+    
+    return [1] + digits
