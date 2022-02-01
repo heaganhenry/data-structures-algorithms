ad7a056428565fc883a24764b7e7da642e63ad92
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-02-01 13:45:30 -0400

    Refactor RemoveElement in python

--- Leetcode/RemoveElement.py
+++ Leetcode/RemoveElement.py
@@ -1,15 +1,16 @@
 # Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
 # Return k after placing the final result in the first k slots of nums.
 
-def removeElement(self, nums, val):
-    if len(nums) == 0:
-        return 0
+class Solution(object):
+    def removeElement(self, nums, val):
+        if len(nums) == 0:
+            return 0
 
-    k = 0
+        k = 0
 
-    for i in range(len(nums)):
-        if nums[i] != val:
-            nums[k] = nums[i]
-            k += 1
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
 
-    return k
+        return k
