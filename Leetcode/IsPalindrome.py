b0fe69176333d2cbc0c4f98d4e04c541a61a6307
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-02-01 13:43:01 -0400

    Refactor IsPalindrome in python

--- Leetcode/IsPalindrome.py
+++ Leetcode/IsPalindrome.py
@@ -1,13 +1,14 @@
 # Given an integer x, return true if x is palindrome integer.
 
-def isPalindrome(self, x):
-        str_num = str(x)
-        str_rev = ""
-        
-        for i in str_num:
-            str_rev = i + str_rev
-        
-        if str_num == str_rev:
-            return True
-        
-        return False
+class Solution(object):
+    def isPalindrome(self, x):
+            str_num = str(x)
+            str_rev = ""
+            
+            for i in str_num:
+                str_rev = i + str_rev
+            
+            if str_num == str_rev:
+                return True
+            
+            return False
