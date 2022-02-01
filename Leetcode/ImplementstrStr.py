a60ea5aa9f0d67f2baa091e2cf42bc0c896f3f1e
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-02-01 13:42:50 -0400

    Refactor ImplementstrStr in python

--- Leetcode/ImplementstrStr.py
+++ Leetcode/ImplementstrStr.py
@@ -1,11 +1,12 @@
 # Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
-def strStr(self, haystack, needle):
-    if len(needle) == 0:
-        return 0
-        
-    for i in range(len(haystack) - len(needle) + 1):
-        if haystack[i:i + len(needle)] == needle:
-            return i
+class Solution(object):
+    def strStr(self, haystack, needle):
+        if len(needle) == 0:
+            return 0
             
-    return -1
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i + len(needle)] == needle:
+                return i
+                
+        return -1
