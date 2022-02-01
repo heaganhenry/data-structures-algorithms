584999e944e35c0c0f2ec7d9d7485af829dc0680
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-02-01 13:44:47 -0400

    Refactor MergeTwoSortedLists in python

--- Leetcode/MergeTwoSortedLists.py
+++ Leetcode/MergeTwoSortedLists.py
@@ -1,22 +1,23 @@
 # You are given the heads of two sorted linked lists list1 and list2.
 # Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
 
-def mergeTwoLists(self, list1, list2):
-        dummy = ListNode()
-        tail = dummy
-        
-        while list1 and list2:
-            if list1.val < list2.val:
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+            dummy = ListNode()
+            tail = dummy
+            
+            while list1 and list2:
+                if list1.val < list2.val:
+                    tail.next = list1
+                    list1 = list1.next
+                else:
+                    tail.next = list2
+                    list2 = list2.next
+                tail = tail.next
+            
+            if list1:
                 tail.next = list1
-                list1 = list1.next
-            else:
+            elif list2:
                 tail.next = list2
-                list2 = list2.next
-            tail = tail.next
-        
-        if list1:
-            tail.next = list1
-        elif list2:
-            tail.next = list2
-        
-        return dummy.next
+            
+            return dummy.next
