9dcc0e3624ba163114e977b23f59527b36767f89
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-02-01 13:43:55 -0400

    Refactor LengthOfLastWord in python

--- Leetcode/LengthOfLastWord.py
+++ Leetcode/LengthOfLastWord.py
@@ -1,12 +1,13 @@
 # Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
 
-def lengthOfLastWord(self, s):
-    i, length = len(s) - 1, 0
-    
-    while s[i] == ' ':
-        i -= 1
-    while i >= 0 and s[i] != ' ':
-        length += 1
-        i -= 1
-    
-    return length
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        i, length = len(s) - 1, 0
+        
+        while s[i] == ' ':
+            i -= 1
+        while i >= 0 and s[i] != ' ':
+            length += 1
+            i -= 1
+        
+        return length
