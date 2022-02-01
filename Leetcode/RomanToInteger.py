16c95ce36ccbe822a0bf4b1856fdfdf8aebfe5e9
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-02-01 13:45:46 -0400

    Refactor RomanToInteger in python

--- Leetcode/RomanToInteger.py
+++ Leetcode/RomanToInteger.py
@@ -1,14 +1,15 @@
 # Given a roman numeral, convert it to an integer.
 
-def romanToInt(s):
-    roman_map = {"I": 1, "V": 5, "X": 10,
-                 "L": 50, "C": 100, "D": 500, "M": 1000}
-    roman_int = 0
+class Solution(object):
+    def romanToInt(s):
+        roman_map = {"I": 1, "V": 5, "X": 10,
+                    "L": 50, "C": 100, "D": 500, "M": 1000}
+        roman_int = 0
 
-    for i in range(len(s)):
-        if i + 1 < len(s) and roman_map[s[i]] < roman_map[s[i + 1]]:
-            roman_int -= roman_map[s[i]]
-        else:
-            roman_int += roman_map[s[i]]
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman_map[s[i]] < roman_map[s[i + 1]]:
+                roman_int -= roman_map[s[i]]
+            else:
+                roman_int += roman_map[s[i]]
 
-    return roman_int
+        return roman_int
