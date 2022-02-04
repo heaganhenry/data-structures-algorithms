92f4c690e2b2cd023dead2742ee97a5c5b6e39db
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-02-04 13:38:18 -0400

    Complete BinaryTreeInorderTraversal in python

---
+++ Leetcode/BinaryTreeInorderTraversal.py
@@ -0,0 +1,9 @@
+# Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        if root is None:
+            return []
+        
+        return self.inorderTraversal(root.left) + [root.val]
+    + self.inorderTraversal(root.right)
