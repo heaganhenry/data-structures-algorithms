0da5318ab7bbbf49bf4f402989b6b69dcf06f5a4
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-02-12 09:36:28 -0400

    Refactor ValidateSubsequence in javascript

--- Algoexpert/ValidateSubsequence.js
+++ Algoexpert/ValidateSubsequence.js
@@ -1,13 +1,12 @@
 // # Given two non-empty arrays of integers, write a function that determines whether the second array is a subsequence of the first one.
 
 function isValidSubsequence(array, sequence) {
-  let i = 0;
-	let j = 0;
-	
-	while (i < array.length && j < sequence.length){
-		if (array[i] === sequence[j]) j++;
-		i++;
-	}
-	
-	return j == sequence.length;
-}
+    let idx = 0;
+      
+      for (const val of array){
+          if (val === sequence[idx]) idx++;
+          if (idx === sequence.length) break;
+      }
+      
+      return idx === sequence.length;
+  }
