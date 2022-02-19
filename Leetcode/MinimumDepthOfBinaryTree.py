e58bf57d68deb6affc9135ecdcc8704140d615de
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-02-19 11:15:35 -0400

    Refactor MinimumDepthOfBinaryTree in python

--- Leetcode/MinimumDepthOfBinaryTree.py
+++ Leetcode/MinimumDepthOfBinaryTree.py
@@ -1,6 +1,17 @@
 # Given a binary tree, find its minimum depth.
 # The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+class Solution(object):
+    def minDepth(self, root):
+        if not root:
+            return 0
+        
+        min_left = self.minDepth(root.left)
+        min_right = self.minDepth(root.right)
+        if min_left == 0 or min_right == 0:
+            return 1 + max(min_left, min_right)
+        return 1 + min(min_left, min_right)
 
+# BFS
 class Solution:
     def minDepth(self, root):
         if not root:
