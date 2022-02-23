bb50ca9045056e8040656c5d41536e5cebbf6c24
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-02-23 11:26:19 -0400

    Refactor PascalsTriangle in python

--- Leetcode/PascalsTriangle.py
+++ Leetcode/PascalsTriangle.py
@@ -1,5 +1,4 @@
 # Given an integer numRows, return the first numRows of Pascal's triangle.
-# In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 
 class Solution(object):
     def generate(self, numRows):
