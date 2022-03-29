c880a9ae7aa12ee102bfb69a416f37e0d3c488a2
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-03-29 11:45:05 -0400

    Refactor TwoSum in python

--- Leetcode/TwoSum.py
+++ Leetcode/TwoSum.py
@@ -1,9 +1,13 @@
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# Time O(n), Space O(n)
 
 class Solution:
     def twoSum(self, nums, target):
-        for i, num1 in enumerate(nums):
-            for j, num2 in enumerate(nums):
-                if i != j and num1 + num2 == target:
-                    return [i, j]
+        hashmap = {}
+        
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in hashmap:
+                return [hashmap[diff], i]
+            hashmap[num] = i
