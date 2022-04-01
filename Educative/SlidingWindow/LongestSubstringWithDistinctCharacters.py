77b225a97504a34aea3282663790d8eaa05a2d44
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-04-01 16:32:51 -0400

    Refactor LongestSubstringWithDistinctCharacters in python

--- Educative/SlidingWindow/LongestSubstringWithDistinctCharacters.py
+++ Educative/SlidingWindow/LongestSubstringWithDistinctCharacters.py
@@ -1,18 +1,15 @@
 # Given a string, find the length of the longest substring, which has all distinct characters.
-# Time O(n), Space O(n)
+# Time O(n), Space O(k) where k is the number of distinct characters in the string
 
 def non_repeat_substring(str1):
-  window_start = 0
-  max_length = 0
-  uniques = set()
+  window_start, max_length = 0, 0
+  index_map = {}
 
   for window_end in range(len(str1)):
-    right_char = str1[window_end]      
-    while right_char in uniques:
-      left_char = str1[window_start]
-      uniques.remove(left_char)
-      window_start += 1
-
-    uniques.add(right_char)
+    right_char = str1[window_end]
+    if right_char in index_map:
+      window_start = max(window_start, index_map[right_char] + 1)
+    
+    index_map[right_char] = window_end
     max_length = max(max_length, window_end - window_start + 1)
   return max_length
