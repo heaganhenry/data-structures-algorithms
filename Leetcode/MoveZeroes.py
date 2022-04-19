304e702e23235d186eab792e71f5c00610c22c56
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-04-19 10:46:22 -0400

    Complete MoveZeroes in python

---
+++ Leetcode/MoveZeroes.py
@@ -0,0 +1,10 @@
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+# Time O(n), Space O(1)
+
+class Solution:
+    def moveZeroes(self, nums):
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0:
+                nums[fast], nums[slow] = nums[slow], nums[fast]
+                slow += 1
