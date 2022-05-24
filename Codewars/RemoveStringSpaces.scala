47d729709972ef75ccbe0ec612efa1a6b2ea421b
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-05-24 10:35:25 -0400

    Delete solutions

--- Codewars/RemoveStringSpaces.scala
+++
@@ -1,7 +0,0 @@
-// Simple, remove the spaces from the string, then return the resultant string.
-// Time O(n), Space O(n)
-
-object Kata {
-  def noSpace(s: String): String =
-    s.filter(_ != ' ')
-}
