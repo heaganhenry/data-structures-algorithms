1a0459033162d527ae7b43ff042910d30918af2b
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-05-28 15:01:41 -0400

    Refactor HelloWorldNTimes in scala

--- Hackerrank/HelloWorldNTimes.scala
+++ Hackerrank/HelloWorldNTimes.scala
@@ -2,11 +2,8 @@
 
 object Solution extends App {
 
-    def f(n :Int): Unit = {
-        def fTailrec(n: Int, acc: String): String =
-            if (n <= 1) acc
-            else fTailrec(n - 1, acc + "\nHello World")
-
-        println(fTailrec(n, "Hello World"))
-  }
+    def f(n : Int): Unit = {
+        println("Hello World")
+        if (n > 1) f(n-1)
+    }
 }
