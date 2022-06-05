2151488f57b36e9f5d78ef7386845607b3eb25ff
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-06-04 21:14:11 -0400

    Complete ReverseList in scala

---
+++ Hackerrank/ReverseList.scala
@@ -0,0 +1,6 @@
+// Given a list of N elements, reverse the list without using the reverse function.
+
+object Solution {
+    def f(arr: List[Int]): List[Int] =
+        arr.foldLeft(List[Int]())((accumulator, value) => value :: accumulator)
+}
