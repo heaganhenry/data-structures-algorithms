57a8a054d05fddd04036da41c527d31cb556952f
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-06-27 19:47:03 -0400

    Refactor ReverseWords in python

--- Codewars/ReverseWords.py
+++ Codewars/ReverseWords.py
@@ -1,8 +1,8 @@
-# 7kyu: Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+# Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
 
 def reverse_words(text):
     result = []
-    for chr in text.split(' '):
-        result.append(chr[::-1])
+    for word in text.split(' '):
+        result.append(word[::-1])
 
     return ' '.join(result)
