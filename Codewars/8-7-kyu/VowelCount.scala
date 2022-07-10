3cf3711e7571d2dd124ebb6063b14a996485df3d
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-07-10 13:13:18 -0400

    Complete VowelCount in scala

---
+++ Codewars/8-7-kyu/VowelCount.scala
@@ -0,0 +1,5 @@
+// Return the number of vowels in the given string ('a', 'e' , 'i', 'o', 'u' but not 'y').
+
+object Sol {
+  def getCount(inputStr: String): Int = inputStr.count("aeiou".contains(_))
+}
