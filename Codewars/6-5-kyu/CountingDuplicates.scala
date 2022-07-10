c94b785af092ef0d8161225c9b3ad55394a847d9
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-07-10 13:27:56 -0400

    Complete CountingDuplicates in scala

---
+++ Codewars/6-5-kyu/CountingDuplicates.scala
@@ -0,0 +1,7 @@
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string.
+// The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+object Dups {
+  def duplicateCount(str: String): Int =
+    str.groupBy(_.toLower).count(_._2.size > 1)
+}
