a4f7ebd8d86bdc86f25cc94b4987f9c6e788909a
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-07-10 13:24:04 -0400

    Complete FilterListPositions in scala

---
+++ Hackerrank/functional-programming/FilterListPositions.scala
@@ -0,0 +1,6 @@
+// For a given list with N integers, return a new list removing the elements at odd positions.
+
+object Solution {
+    def f(arr: List[Int]): List[Int] =
+        arr.zipWithIndex.filter(_._2 % 2 != 0).map(_._1)
+}
