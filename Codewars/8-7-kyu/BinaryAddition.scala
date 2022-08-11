4fd1d9db16b8b45c34a688cd434e7513102cb11e
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-08-10 21:59:17 -0400

    Complete BinaryAddition in scala

---
+++ Codewars/8-7-kyu/BinaryAddition.scala
@@ -0,0 +1,5 @@
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+def addBinary(a: Int, b: Int): String = {
+  (a + b).toBinaryString
+}
