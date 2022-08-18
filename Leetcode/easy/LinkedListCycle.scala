2ae08bc0b224335d2b33ddd36dc1317a0db4e10c
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-08-18 12:47:54 -0400

    Refactor LinkedListCycle in scala

--- Leetcode/easy/LinkedListCycle.scala
+++ Leetcode/easy/LinkedListCycle.scala
@@ -1,15 +1,16 @@
 // Given the head of a linked list, determine if the linked list has a cycle in it.
-// Time O(n), Space O(n)
+// Time O(n), Space O(1)
 
 object Solution {
     def hasCycle(head: ListNode): Boolean = {
         @annotation.tailrec
-        def hasCycleRec(node: ListNode, set: Set[ListNode]): Boolean = {
-            if (node == null) false
-            else if(set.contains(node)) true
-            else hasCycleRec(node.next, set + node)
+        def hasCycleRec(slow: ListNode, fast: ListNode): Boolean = {
+            if (fast == null || fast.next == null) false
+            else if (slow == fast) true
+            else hasCycleRec(slow.next, fast.next.next)
         }
 
-        hasCycleRec(head, Set())
+        if (head == null || head.next == null) false
+        else hasCycleRec(head, head.next)
     }
 }
