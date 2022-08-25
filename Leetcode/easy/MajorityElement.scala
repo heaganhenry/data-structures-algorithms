5e09c2ed9df34ffc695f394a97231f4a036c03e8
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-08-25 13:52:31 -0400

    Refactor MajorityElement in scala

--- Leetcode/easy/MajorityElement.scala
+++ Leetcode/easy/MajorityElement.scala
@@ -1,10 +1,12 @@
 // Given an array nums of size n, return the majority element. The majority element is the element that appears more than ⌊n / 2⌋ times.
-// Time O(n), Space O(n)
+// Time O(n), Space O(1)
 
 object Solution {
     def majorityElement(nums: Array[Int]): Int = {
-        val length = nums.length
-        val freqMap = nums.groupBy(identity).view.mapValues(_.length).toMap
-        nums.find(freqMap(_) > length / 2).getOrElse(-1)
+        nums.foldLeft((0, 0)) { case ((res, count), num) => {
+            val r = if (count == 0) num else res
+            val c = if (r == num) count + 1 else count - 1
+            (r, c)
+        }}._1
     }
 }
