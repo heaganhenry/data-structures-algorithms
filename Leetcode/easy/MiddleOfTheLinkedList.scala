112685a0b280f5af4444a1a4608e92003cc9e355
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-09-04 06:13:14 -0400

    Refactor MiddleOfTheLinkedList in scala

--- Leetcode/easy/MiddleOfTheLinkedList.scala
+++ Leetcode/easy/MiddleOfTheLinkedList.scala
@@ -3,17 +3,11 @@
 
 object Solution {
     def middleNode(head: ListNode): ListNode = {
-        def countNodes(head: ListNode, count: Int): Int = {
-            if (head == null) count
-            else countNodes(head.next, count + 1)
+        def getMiddle(slow: ListNode, fast: ListNode): ListNode = {
+            if (fast == null || fast.next == null) slow
+            else getMiddle(slow.next, fast.next.next)
         }
 
-        def getMiddle(node: ListNode, count: Int, current: Int): ListNode = {
-            if (current == count / 2) node
-            else getMiddle(node.next, count, current + 1)
-        }
-
-        val count = countNodes(head, 0)
-        getMiddle(head, count, 0)
+        getMiddle(head, head)
     }
 }
