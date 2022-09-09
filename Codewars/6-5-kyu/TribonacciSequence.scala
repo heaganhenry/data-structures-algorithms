c7fdaa797d661c176994a7fa7e457876feb24413
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-09-09 17:57:49 -0400

    Refactor TribonacciSequence in scala

--- Codewars/6-5-kyu/TribonacciSequence.scala
+++ Codewars/6-5-kyu/TribonacciSequence.scala
@@ -2,11 +2,6 @@
 // Tribonnaci is created using the sum of the last 3 numbers of the sequence to generate the next e.g. [1, 1 ,1, 3, 5, 9, 17, 31]
 
 object Tribonacci {
-  def tribonacci[T: Numeric](signature: List[T], n: Int): List[T] = {
-    if (n <= 3) signature.take(n)
-    else {
-      val prev = tribonacci(signature, n - 1)
-      prev ::: List(prev.reverse.take(3).sum)
-    }
-  }
+  def tribonacci[T : Numeric](sig: List[T], n: Int): List[T] =
+    if (n <= 3) sig take n else sig.head +: tribonacci(sig.tail :+ sig.sum, n - 1)
 }
