abab513b8e59a111530308d77709d0de31be54f2
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-09-15 11:04:05 -0400

    Refactor SearchA2DMatrix in scala

--- Leetcode/medium/SearchA2DMatrix.scala
+++ Leetcode/medium/SearchA2DMatrix.scala
@@ -1,17 +1,21 @@
 // Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
 // 1. Integers in each row are sorted from left to right. 2. The first integer of each row is greater than the last integer of the previous row.
-// Time O(n), Space O(1)
+// Time O(log(m * n), Space O(1)
 
 object Solution {
     def searchMatrix(matrix: Array[Array[Int]], target: Int): Boolean = {
-        var outerIdx = 0
-        while (outerIdx < matrix.length) {
-            for (innerIdx <- matrix(0).indices) {
-                if (matrix(outerIdx)(innerIdx) == target) return true
-            }
-            outerIdx += 1
+        val rows = matrix.length
+        val cols = matrix.head.length
+
+        @annotation.tailrec
+        def searchRec(left: Int, right: Int): Boolean = {
+            val mid = (left + right) / 2
+            if (left == right) false
+            else if (matrix(mid / cols)(mid % cols) > target) searchRec(left, mid)
+            else if (matrix(mid / cols)(mid % cols) < target) searchRec(mid + 1, right)
+            else true
         }
 
-        return false
+        searchRec(0, cols * rows)
     }
 }
