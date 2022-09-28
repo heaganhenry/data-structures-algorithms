b906fb215052fe667eb74a36fdc27a946ffc95cb
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-09-28 07:14:40 -0400

    Refactor VowelCount in javascript

--- Codewars/8-7-kyu/VowelCount.js
+++ Codewars/8-7-kyu/VowelCount.js
@@ -1,13 +1,5 @@
 // Return the number of vowels in the given string ('a', 'e' , 'i', 'o', 'u' but not 'y').
 
 function getCount(str) {
-    const vowels = "aeiou";
-    let count = 0;
-
-    for (chr of str) {
-      if (vowels.includes(chr))
-        count++;
-    }
-
-    return count;
+  return str.split('').filter(c => "aeiou".includes(c)).length
 }
