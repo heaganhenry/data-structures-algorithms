b04e08f2ebdd65f065578fbfe75147d1ddba8181
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-10-07 07:02:08 -0400

    Delete solution

--- Codewars/8-7-kyu/DisemvowelTrolls.js
+++
@@ -1,5 +0,0 @@
-// Trolls are attacking your comment section! Remove all of the vowels from the trolls' comments, neutralizing the threat.
-
-function disemvowel(str) {
-    return str.replace(/[aeiou]/gi, '');
-}
