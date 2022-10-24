4be171a16381dfc025d7790cb485e2ab48de7020
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-10-24 11:03:29 -0400

    Refactor MajorityElement in javascript

--- Leetcode/easy/MajorityElement.js
+++ Leetcode/easy/MajorityElement.js
@@ -1,11 +1,14 @@
 // Given an array nums of size n, return the majority element. The majority element is the element that appears more than ⌊n / 2⌋ times.
-// Time O(n), Space O(n)
+// Time O(n), Space O(1)
 
 const majorityElement = function(nums) {
-    let count = {}
+    let result = 0
+    let count = 0
 
-    for (let n of nums) {
-        count[n] ? count[n]++ : count[n] = 1
-        if (count[n] > nums.length / 2) return n
+    for (let i = 0; i < nums.length; i++) {
+        result = (count === 0) ? nums[i] : result
+        count += (result === nums[i]) ? 1 : -1
     }
+
+    return result
 }
