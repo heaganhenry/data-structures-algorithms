97c7d63187e03836d4095f0f59af3e88e4afca16
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-10-30 07:47:16 -0400

    Add both MergeTwoSortedLists solutions in javascript

--- Leetcode/easy/MergeTwoSortedLists.js
+++ Leetcode/easy/MergeTwoSortedLists.js
@@ -1,8 +1,29 @@
 // You are given the heads of two sorted linked lists list1 and list2. Merge the two lists in a one sorted list.
 // The list should be made by splicing together the nodes of the first two lists. Return the head of the merged linked list.
-// Time O(min(m, n)), Space O(min(m, n))
+// Iterative: Time O(min(m, n)), Space O(1)
 
 const mergeTwoLists = function (list1, list2) {
+    let head = new ListNode(-1, null);
+    let curr = head;
+
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            curr.next = list1;
+            list1 = list1.next;
+        } else {
+            curr.next = list2;
+            list2 = list2.next;
+        }
+        curr = curr.next;
+    }
+    curr.next = (list1 || list2);
+
+    return head.next;
+};
+
+// Tail Recursive: Time O(min(m, n)), Space O(min(m, n))
+
+const mergeTwoListsRec = function (list1, list2) {
     if (!list1) return list2;
     if (!list2) return list1;
 
